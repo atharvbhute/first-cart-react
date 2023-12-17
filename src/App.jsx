fb1f3492119e2d2132ae74a9e31fdcb99e63de95
Atharv Bhute <atharv.bhute18@gmail.com>
2023-12-17 16:29:44 +0530

    service - appwrite auth

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Sidebar } from './components/index'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Sidebar />
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf = {
+    appwriteURL : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionIdAddress: String(import.meta.VITE_APPWRITE_COLLECTION_ADDRESS),
+    appwriteCollectionIdProducts : String(import.meta.VITE_APPWRITE_COLLECTION_PRODUCTS),
+    appwriteCollectionIdCategories : String(import.meta.VITE_APPWRITE_COLLECTION_CATEGORIES),
+    appwriteCollectionIdCart : String(import.meta.VITE_APPWRITE_COLLECTION_CART),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
