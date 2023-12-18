e06048d6f22e4aeb8dd82d104c0cab8cad020a50
Atharv Bhute <atharv.bhute18@gmail.com>
2023-12-19 02:08:36 +0530

    service - added - create product

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import { Sidebar } from './components/index'
+import { productService } from './appwrite/productService';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState("df");
+
+  function createProduct(){
+    productService.createProduct({
+      name : "fridge",
+      description : "lorem ipsum gebrish",
+      brand : "worlpool",
+      category_id : "sdasdf35asd2",
+      image : "aasdfadflskd254555"
+    }).then((response)=> setData(response)).catch((err)=> alert(err));
+  }
 
   return (
     <>
-    <Sidebar />
+    <div>
+      <button onClick={createProduct}>click to add</button>
+      <p>{data.name}</p>
+    </div>
      
     </>
   )
--- src/appwrite/productService.js
+++ src/appwrite/productService.js
@@ -1,6 +1,36 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, ID } from "appwrite";
+import conf from "../conf/conf";
 // adding products
 // edit products
 // delete products
 // displaying products
 
+class ProductService {
+  client = new Client();
+  databases;
+
+  constructor() {
+    console.log(conf.appwriteURL, conf.appwriteProjectId);
+    this.client
+      .setEndpoint(conf.appwriteURL)
+      .setProject(conf.appwriteProjectId);
+
+    this.databases = new Databases(this.client);
+
+  }
+
+  async createProduct({ name, brand, category_id, description , image }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionIdProducts,
+        ID.unique(),
+        { name, brand, category_id, image, description}
+      );
+    } catch (error) {
+        console.log(error);
+    }
+  }
+}
+
+export const productService = new ProductService();
